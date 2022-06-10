ddd901d8a6d1fd00d04fa4527ad1d030d2bb78cf
AllanT102 <allan.zf.tan@gmail.com>
2022-06-10 00:50:04 -0700

    Completed project

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const paperBtn = document.querySelector('.paper');
 const sciBtn = document.querySelector('.scissors');
 const playBtn = document.querySelector('.play');
 const overlay = document.querySelector('.play-button')
+const cputext = document.querySelector('.cpu');
+const playertext = document.querySelector('.player');
+const title = document.querySelector('.title');
+
+var cpuscore = 0;
+var playerScore = 0;
 
 playBtn.addEventListener('click', () => {
     overlay.classList.toggle("hide");
+    playBtn.disabled = 'disabled';
     console.log('hi')
 });
 
@@ -32,6 +39,7 @@ function play(userChoice, computerPlay) {
     userChoice = userChoice.toLowerCase();
     let computerChoice = computerPlay;
     if (userChoice === computerChoice) {
+        console.log('tie')
         displayResult("tie", null);
     } else if (userChoice === "paper" && computerChoice === "rock") {
         displayResult("paper", "rock");
@@ -47,16 +55,15 @@ function play(userChoice, computerPlay) {
 function displayResult(user, cpu) {
     let result = "";
     if (user === "tie") {
-        console.log("tie");
+        title.textContent = "It was a tie.";
     } else if (user === "loss") {
-        console.log("loss");
+        cpuscore++;
+        title.textContent = "You lost.";
+        cputext.textContent = `Computer Score: ${cpuscore}`;
     } else {
-        console.log("win");
+        playerScore++;
+        title.textContent = "You won.";
+        playertext.textContent = `Player Score: ${playerScore}`;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(play(userChoice, computerPlay));
-    }
-}
