f7880797c91495fc211f2dfcddf1a5312191726c
AllanT102 <allan.zf.tan@gmail.com>
2022-06-09 23:52:09 -0700

    Add initial play screen

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
-// let choices = ["rock", "paper", "scissors"];
-// // let userChoice = document.querySelector("userInputer");
-// let userChoice = prompt("Choose rock, paper, or scissors. Be wise");
+let choices = ["rock", "paper", "scissors"];
 
-// function computerPlay() {
-//     let choice = Math.floor(Math.random() * 2);
-//     return choices[choice];
-// }
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const sciBtn = document.querySelector('.scissors');
+const playBtn = document.querySelector('.play');
+const overlay = document.querySelector('.play-button')
 
-// function play(userChoice, computerPlay) {
-//     userChoice = userChoice.toLowerCase();
-//     let computerChoice = computerPlay;
-//     if (userChoice === computerChoice) {
-//         displayResult("tie", null);
-//     } else if (userChoice === "paper" && computerChoice === "rock") {
-//         displayResult("paper", "rock");
-//     } else if (userChoice === "rock" && computerChoice === "scissors") {
-//         displayResult("rock", "scissors");
-//     } else if (userChoice === "scissors" && computerChoice === "paper") {
-//         displayResult("scissors", "paper");
-//     } else {
-//         displayResult("loss", null);
-//     }
-// }
+playBtn.addEventListener('click', () => {
+    overlay.classList.toggle("hide");
+    console.log('hi')
+});
 
-// function displayResult(user, cpu) {
-//     let result = "";
-//     if (user === "tie") {
-//         return result + "The game was a tie";
-//     } else if (user === "loss") {
-//         return result + "Unlucky, the computer beat you... Better luck next time.";
-//     } else {
-//         return result + `Lucky you... you beat a computer. You picked ${user}, and that beats ${cpu}`;
-//     }
-// }
+const btnPlay = event => {
+    const btnClass = event.target.classList[1];
+    if (btnClass == "rock") play('rock', computerPlay());
+    else if (btnClass == "paper") play('paper', computerPlay());
+    else play('scissors', computerPlay());
+}
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         console.log(play(userChoice, computerPlay));
-//     }
-// }
+rockBtn.addEventListener('click', btnPlay);
+paperBtn.addEventListener('click', btnPlay);
+sciBtn.addEventListener('click', btnPlay);
+
+
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+}
+
+function play(userChoice, computerPlay) {
+    userChoice = userChoice.toLowerCase();
+    let computerChoice = computerPlay;
+    if (userChoice === computerChoice) {
+        displayResult("tie", null);
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+        displayResult("paper", "rock");
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+        displayResult("rock", "scissors");
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+        displayResult("scissors", "paper");
+    } else {
+        displayResult("loss", null);
+    }
+}
+
+function displayResult(user, cpu) {
+    let result = "";
+    if (user === "tie") {
+        console.log("tie");
+    } else if (user === "loss") {
+        console.log("loss");
+    } else {
+        console.log("win");
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(play(userChoice, computerPlay));
+    }
+}
